a788aa8c2412613915421a1e681d6f61163a4261
Kittipos <ki11ip0.s.a.s@gmail.com>
2023-05-27 23:50:52 +0700

    First Commit

---
+++ app/main.py
@@ -0,0 +1,13 @@
+# from shiny import Inputs, Outputs, Session, App, reactive, render, req, ui
+# from stroke_ct_mod import stroke_ct_ui, stroke_ct_server
+
+# app_ui = ui.page_fluid(
+#     stroke_ct_ui("1")
+# )
+
+
+# def server(input: Inputs, output: Outputs, session: Session):
+#     stroke_ct_server("1")
+
+
+# app = App(app_ui, server)
